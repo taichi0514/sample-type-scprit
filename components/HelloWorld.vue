26f0143dddb70d48d37103a4b896817271dcde5b
taichi.murai <taichimurai0514@gmail.com>
2019-05-19 23:08:37 +0900

    初期コミット

---
+++ components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+  <ul>
+    <li>{{message}}</li>
+    <li>{{number}}</li>
+    <li>{{title}}</li>
+    <li>{{description}}</li>
+    <li>{{typo}}</li>
+    <label for>
+      <input v-model="meg">
+      <button @click="onClick(meg)">change</button>
+    </label>
+  </ul>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-property-decorator";
+@Component
+export default class HelloWorld extends Vue {
+  @Prop({ type: String })
+  message: string = "Hello world !";
+  number: number = 514;
+  title: string = "wifi";
+  description: string = "説明";
+  typo: string = "タイポじゃ";
+  onClick(text) {
+    this.message = text;
+  }
+}
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,7 @@
+import NuxtConfiguration from "@nuxt/config";
+
+const config: NuxtConfiguration = {
+  // タイプするか `Ctrl + Space` を押すとオートコンプリートできます
+};
+
+export default config;
